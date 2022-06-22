d4e4baa9f184520a70b187a13295772b4b560c5b
Douglas Anaya <102629422+Zaindou@users.noreply.github.com>
2022-06-22 17:42:00 -0500

    Implementación diseño.

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Post(models.Model):
         title = models.CharField(max_length=20)
         content = models.TextField(max_length=300)
+        updated_on = models.DateTimeField(auto_now= True)
+        created_on = models.DateTimeField(auto_now_add=True)
 
             
         def __str__(self):
             return self.title
-    
+        
+        class Meta:
+            ordering = ['-created_on']
 
